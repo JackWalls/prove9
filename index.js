ef4972e0fc089f1af4d764bd4e558ffbee5ad80a
Jack Walls <josejoaquinparedes0@gmail.com>
2019-06-22 15:25:23 -0700

    test

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const path = require('path')
+var app = express()
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, '/public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(__dirname + '/public'))
+
+app.listen(PORT)
+
